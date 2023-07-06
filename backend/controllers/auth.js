751eedf86d715d33a0418a55ef2c07a3ee0cf593
khouloudAl2022 <aloui.khouloud11@gmail.com>
2023-07-06 12:44:52 +0100

    Encrypt user passwords for enhanced security

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,10 +1,13 @@
 const User = require("../models/User");
-
+const CryptoJS = require("crypto-js");
 exports.RegisterUser = async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SECRET
+    ).toString(),
   });
   try {
     const savedUser = await newUser.save();
