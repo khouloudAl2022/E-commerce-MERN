d4611184e020cab8067626a7c6d71c56e1a5ce8c
khouloudAl2022 <aloui.khouloud11@gmail.com>
2023-07-06 17:54:50 +0100

    prevent exposing sensitive information(password) in the response

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+//Register
 exports.RegisterUser = async (req, res) => {
   const newUser = new User({
     username: req.body.username,
@@ -12,6 +13,30 @@ exports.RegisterUser = async (req, res) => {
   try {
     const savedUser = await newUser.save();
     res.status(201).send(savedUser);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+//login
+
+exports.Login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(401).send("Wrong credentials!");
+    }
+
+    const hashPass = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SECRET
+    );
+
+    const originalPassword = hashPass.toString(CryptoJS.enc.Utf8);
+    if (originalPassword !== req.body.password) {
+      return res.status(401).send("Wrong credentials!");
+    }
+    const { password, ...others } = user._doc;
+    res.status(200).send({ msg: "success login", others });
   } catch (error) {
     res.status(500).send(error);
   }
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,11 @@
-const { RegisterUser } = require("../controllers/auth");
+const { RegisterUser, Login } = require("../controllers/auth");
 
 const router = require("express").Router();
 
 //Register user
 
 router.post("/register", RegisterUser);
+//Login
+router.post("/login", Login);
 
 module.exports = router;
