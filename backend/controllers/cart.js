50774c74a8b51cd0236e281b3c28859faff0db09
khouloudAl2022 <aloui.khouloud11@gmail.com>
2023-07-11 13:04:37 +0100

    feat:Add getAllCart eendpoint

--- backend/controllers/cart.js
+++ backend/controllers/cart.js
@@ -43,27 +43,12 @@ exports.GetUserCart = async (req, res) => {
     res.status(500).send("server error");
   }
 };
-// //GET ALL PRODUCTS
-// exports.GetAllProduct = async (req, res) => {
-//   const qNew = req.query.new;
-//   const qCategory = req.query.category;
-//   try {
-//     let products;
-
-//     if (qNew) {
-//       products = await Product.find().sort({ createdAt: -1 }).limit(1);
-//     } else if (qCategory) {
-//       products = await Product.find({
-//         categories: {
-//           $in: [qCategory],
-//         },
-//       });
-//     } else {
-//       products = await Product.find();
-//     }
-
-//     res.status(200).json(products);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// };
+//GET ALL
+exports.GetAllCart = async (req, res) => {
+  try {
+    const carts = await Cart.find();
+    res.status(200).send(carts);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -3,10 +3,12 @@ const {
   EditCart,
   DeleteCart,
   GetUserCart,
+  GetAllCart,
 } = require("../controllers/cart");
 const {
   verifyTokenAndAuth,
   verifyToken,
+  verifyTokenAndAdmin,
 } = require("../middlewars/verifyToken");
 
 const router = require("express").Router();
@@ -15,6 +17,6 @@ router.post("/", verifyToken, CreateCart);
 router.put("/:id", verifyTokenAndAuth, EditCart);
 router.delete("/:id", verifyTokenAndAuth, DeleteCart);
 router.get("/find/:userId", verifyTokenAndAuth, GetUserCart);
-// router.get("/", GetAllProduct);
+router.get("/", verifyTokenAndAdmin, GetAllCart);
 
 module.exports = router;
