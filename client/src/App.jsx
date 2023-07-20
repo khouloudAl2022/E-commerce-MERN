26982a1eaf0478d1c607d076cb3cac35451c588f
khouloudAl2022 <aloui.khouloud11@gmail.com>
2023-07-21 00:54:36 +0100

    Add Navbar component and styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Success from "./pages/Success";
 import Cart from "./pages/Cart";
 import PayButton from "./pages/PayButton";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
+import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/pay" element={<PayButton></PayButton>} />{" "}
+          <Route exact path="/" element={<Home></Home>} />
+          <Route path="/pay" element={<PayButton></PayButton>} />
           <Route
             path="/products/:category"
             element={<ProductList></ProductList>}
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { styled } from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  height: 60px;
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-item: center;
+`;
+const Language = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+const Input = styled.input`
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
+`;
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+const Right = styled.div`
+flex: 1;
+display: flex;
+align-items: center;
+justify-content: flex-end;
+*`;
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+
+          <SearchContainer>
+            <Input placeholder="Search" />
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              style={{ color: "#bdbdbd" }}
+            />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>KikiShop.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <Link to="/cart">
+            <MenuItem>
+              <FontAwesomeIcon
+                icon={faCartShopping}
+                style={{ color: "#292f30", fontSize: "30px" }}
+              />
+            </MenuItem>
+          </Link>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
